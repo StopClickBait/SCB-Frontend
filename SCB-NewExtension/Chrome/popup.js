7c8555ef21bf38f9d9631a556aa5dcd7980327f5
Noah Allen <noahtallen@gmail.com>
2017-04-24 11:38:44 -0400

    More setup for the popup menu
    Added more buttons and functions to handle button clicks for myposts.
    Added basic function for login.

--- SCB-NewExtension/Chrome/popup.js
+++ SCB-NewExtension/Chrome/popup.js
@@ -1,1 +1,28 @@
-﻿
+﻿function statusChangeCallback(response) {
+    console.log("statusChangeCallback");
+    console.log(response);
+}
+
+var loggedIn = true;
+
+document.getElementById("YourPosts").style.display = "none";
+
+if(loggedIn)
+{
+    document.getElementById("Profile_Logged_Out").style.display = "none";
+    document.getElementById("Profile_Logged_In").style.display = "block";
+    addEventHandlers();
+    
+} else {
+    document.getElementById("Profile_Logged_In").style.display = "none";
+    document.getElementById("Profile_Logged_Out").style.display = "block";
+}
+
+function addEventHandlers() {
+    var postsBttn = document.getElementById("postsBttn");
+    postsBttn.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.getElementById("YourPosts").style.display = "block";
+        document.getElementById("Profile_Logged_In").style.display = "none";
+    })
+}
