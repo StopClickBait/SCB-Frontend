40058d420bcc355c44cdf5dae30b3ca8ffbf3caf
sidneyjohn23 <sidneyjohn@hotmail.de>
2017-04-25 23:12:09 +0200

    Localization (but not working yet)

--- SCB-NewExtension/Chrome/SCB-Container.js
+++ SCB-NewExtension/Chrome/SCB-Container.js
@@ -281,6 +281,7 @@ function createCommentBox(commentId, timestamp, content, userNameString, voteNum
     reportLink.classList.add('reportLink');
 
     reportLinkA.href = "#";
+    reportLinkA.setAttribute('data-localize', 'report');
     reportLinkA.innerText = "report";
 
     voteArea.classList.add('voteArea');
@@ -299,9 +300,11 @@ function createCommentBox(commentId, timestamp, content, userNameString, voteNum
     deleteButtons.onmouseover = function () { return false; };
 
     deleteButton.classList.add('deleteButton');
+    deleteButton.setAttribute('data-localize', 'delete');
     deleteButton.innerText = 'Delete';
 
     cancelButton.classList.add('cancelButton');
+    cancelButton.setAttribute('data-localize', 'cancel');
     cancelButton.innerText = 'Cancel';
 
 }
---
+++ SCB-NewExtension/Edge/i18n.js
@@ -0,0 +1,40 @@
+﻿function replace_i18n(obj, tag) {
+    var msg = tag.replace(/__MSG_(\w+)__/g, function (match, v1) {
+        console.log(chrome.i18n.getMessage(v1));
+        return v1 ? chrome.i18n.getMessage(v1) : '';
+    });
+
+    if (msg != tag) obj.innerHTML = msg;
+}
+
+function localizeHtmlPage() {
+    // Localize using __MSG_***__ data tags
+    var data = document.querySelectorAll('[data-localize]');
+
+    for (var i in data) if (data.hasOwnProperty(i)) {
+        var obj = data[i];
+        var tag = obj.getAttribute('data-localize').toString();
+
+        replace_i18n(obj, tag);
+    }
+
+    var data2 = document.querySelectorAll('[data-localize-placeholder]');
+    var obj2 = data2[0];
+    var tag2 = obj2.getAttribute('data-localize-placeholder').toString();
+    var msg2 = tag2.replace(/__MSG_(\w+)__/g, function (match, v1) {
+        return v1 ? chrome.i18n.getMessage(v1) : '';
+    });
+    obj2.setAttribute('placeholder', msg2);
+
+    // Localize everything else by replacing all __MSG_***__ tags
+    var page = document.getElementsByTagName('body');
+
+    for (var j = 0; j < page.length; j++) {
+        var obj = page[j];
+        var tag = obj.innerHTML.toString();
+
+        replace_i18n(obj, tag);
+    }
+}
+
+localizeHtmlPage();
