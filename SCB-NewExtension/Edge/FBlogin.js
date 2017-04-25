6477ac95f6309ad5abcd335a7406905e55825718
sidneyjohn23 <sidneyjohn@hotmail.de>
2017-04-25 21:47:25 +0200

    Facebook login works! (hopefully)

---
+++ SCB-NewExtension/Edge/FBlogin.js
@@ -0,0 +1,4 @@
+﻿var accessToken = window.location.hash;
+accessToken = accessToken.substring(accessToken.indexOf('=') + 1, accessToken.indexOf('&'));
+
+chrome.storage.local.set({ 'accessToken': accessToken }, () => { window.close() });
