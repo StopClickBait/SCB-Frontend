febc03d8e526183ce7ad9dfe3320185ebddda5a2
sidneyjohn23 <sidneyjohn@hotmail.de>
2017-04-26 16:03:45 +0200

    Internationalization works! (German, English)

--- SCB-NewExtension/Chrome/i18n.js
+++ SCB-NewExtension/Chrome/i18n.js
@@ -1,40 +1,27 @@
 ﻿function replace_i18n(obj, tag) {
-    var msg = tag.replace(/__MSG_(\w+)__/g, function (match, v1) {
-        console.log(chrome.i18n.getMessage(v1));
-        return v1 ? chrome.i18n.getMessage(v1) : '';
-    });
-
-    if (msg != tag) obj.innerHTML = msg;
+    var m = tag;
+    var m2 = chrome.i18n.getMessage(m);
+    if (m2 != tag) obj.innerHTML = m2;
 }
 
 function localizeHtmlPage() {
-    // Localize using __MSG_***__ data tags
     var data = document.querySelectorAll('[data-localize]');
 
     for (var i in data) if (data.hasOwnProperty(i)) {
         var obj = data[i];
         var tag = obj.getAttribute('data-localize').toString();
-
         replace_i18n(obj, tag);
     }
 
     var data2 = document.querySelectorAll('[data-localize-placeholder]');
-    var obj2 = data2[0];
+    for (var i in data2) if (data2.hasOwnProperty(i)) {
+    var obj2 = data2[i];
     var tag2 = obj2.getAttribute('data-localize-placeholder').toString();
-    var msg2 = tag2.replace(/__MSG_(\w+)__/g, function (match, v1) {
-        return v1 ? chrome.i18n.getMessage(v1) : '';
-    });
+    console.log(tag2);
+    var msg2 = chrome.i18n.getMessage(tag2);
     obj2.setAttribute('placeholder', msg2);
-
-    // Localize everything else by replacing all __MSG_***__ tags
-    //var page = document.getElementsByTagName('body');
-
-    for (var j = 0; j < page.length; j++) {
-        var obj = page[j];
-        var tag = obj.innerHTML.toString();
-
-        replace_i18n(obj, tag);
     }
+
 }
 
 localizeHtmlPage();
