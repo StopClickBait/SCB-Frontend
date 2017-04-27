615d605af6ca50dd3b1fc954cae457bc5b325e85
Noah Allen <noahtallen@gmail.com>
2017-04-27 15:13:28 -0400

    Changes to button styles

--- SCB-NewExtension/Chrome/popup.js
+++ SCB-NewExtension/Chrome/popup.js
@@ -1,5 +1,4 @@
 ﻿var colors = ["#3b5999", "#00acee","#c83b6f","#1bbc9b","#34495e","#e84c3d","#2dcc70","#9b58b5"];
-var selectedColor;
 
 var content = null;
 var userToken;
@@ -7,37 +6,36 @@ var userToken;
 document.getElementById("YourPosts").style.display = "none";
 document.getElementById("Profile_Logged_In").style.display = "none";
 document.getElementById("Profile_Logged_Out").style.display = "block";
-
-var loginButton = document.getElementById("LoginButton");
-loginButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    var win = window.open('https://www.facebook.com/v2.9/dialog/oauth?client_id=137575509998503&response_type=token&redirect_uri=https://www.facebook.com/connect/login_success.html');
-});
+document.getElementById("settings").style.display = "none";
 
 setupColors();
 
-chrome.storage.local.get('selectedColor', function (items) {
-    for(var prop in items) {
-        if(items.hasOwnProperty(prop))
-        {
-            // Move the "selected" style to the saved color
-            changeSelectedStyleTo(document.getElementById(items.selectedColor));
-            // Update the local colors
-            setElementColors(items.selectedColor);
-            return;
-        }
-    }
-    // If there is no setting for selectedColor - i.e. the first time popup.html is opened:
-    chrome.storage.local.set({'selectedColor': colors[0]}, function() {
-        console.log(colors[0] + " saved to default.");
-    });
-    changeSelectedStyleTo(document.getElementById(colors[0]));
-    setElementColors(colors[0]);
-})
+addEventHandlers();
 
+// Setup access token for facebook:
+chrome.storage.local.get('accessToken', (accessToken) => {
+    userToken = accessToken.accessToken;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://graph.facebook.com/v2.9/me?access_token=' + accessToken.accessToken + '&fields=id%2Cname');
+    xhr.send(null);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == xhr.DONE) {
+            content = JSON.parse(xhr.responseText);
+            if (content.error) {
+                // Not logged in.
+            } else {
+                chrome.storage.local.set({ 'user_id': content.id });
+                processLogIn();
+            }
+        }
+    };
+});
+
+////* LOGIN FUNCTIONS: *////
 function processLogIn() {
     document.getElementById("Profile_Logged_Out").style.display = "none";
     document.getElementById("Profile_Logged_In").style.display = "block";
+    document.getElementById("settings").style.display = "block";
     document.getElementById('profile_name').innerText = content.name;
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://graph.facebook.com/v2.9/me/picture?access_token=' + userToken + '&redirect=false&type=square');
@@ -52,10 +50,12 @@ function processLogIn() {
             }
         }
     }
-    addEventHandlers();
+    
 }
 
+////* SETUP FUNCTION: *////
 function addEventHandlers() {
+    // Add handler to posts button:
     var postsBttn = document.getElementById("postsBttn");
     postsBttn.addEventListener("click", function (e) {
         e.preventDefault();
@@ -64,78 +64,32 @@ function addEventHandlers() {
         document.getElementById("settings").style.display = "none";
     })
 
+    // Add handler to changed color in storage:
     chrome.storage.onChanged.addListener(function(changes, namespace) {
         if(namespace === "local" && changes["selectedColor"]) {
             setElementColors(changes["selectedColor"].newValue);
         }
     });
+
+    // Add handler to login button:
+    var loginButton = document.getElementById("LoginButton");
+    loginButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        var win = window.open('https://www.facebook.com/v2.9/dialog/oauth?client_id=137575509998503&response_type=token&redirect_uri=https://www.facebook.com/connect/login_success.html');
+    });
+
+    var logoutButton = document.getElementById("login");
+    logoutButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.getElementById("Profile_Logged_Out").style.display = "block";
+        document.getElementById("Profile_Logged_In").style.display = "none";
+        document.getElementById("settings").style.display = "none";
+
+        // Need to do more stuff for logout, including resize window.
+    });
 }
 
-function setupColors() {
-    for(i = 0; i < colors.length; i++) {
-        var colorDiv = document.createElement("div");
-        colorDiv.className = "colorButton";
-        colorDiv.id = colors[i];
-        colorDiv.style.backgroundColor = colors[i];
-        colorDiv.addEventListener("click", function(e) {
-            e.preventDefault();
-            var caller = e.target || e.srcElement;
-
-            // Set the selected color to the same as the background color. 
-            selectedColor = rgb2hex(caller.style.backgroundColor);
-            chrome.storage.local.set({'selectedColor': selectedColor}, function() {
-                console.log(selectedColor + " saved to default.");
-            });
-            changeSelectedStyleTo(caller);    
-        })
-        if(i == 0) {
-            selectedColor = colors[i];
-            colorDiv.className += " selectedColorButton";
-        }
-        document.getElementById("colors").appendChild(colorDiv);
-    }
-}
-
-function changeSelectedStyleTo(element) {
-        // Remove selection styling:
-        var colors = document.getElementById("colors").getElementsByTagName("div");
-        for (i = 0; i < colors.length; i++)
-        {
-            if(colors[i].classList.contains("selectedColorButton"))
-                colors[i].classList.remove("selectedColorButton");
-        }
-        // Add selection styling to selected div:
-        element.className += " selectedColorButton";
-}
-
-chrome.storage.local.get('accessToken', (accessToken) => {
-    userToken = accessToken.accessToken;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://graph.facebook.com/v2.9/me?access_token=' + accessToken.accessToken + '&fields=id%2Cname');
-    xhr.send(null);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == xhr.DONE) {
-            content = JSON.parse(xhr.responseText);
-            if (content.error) {
-
-            } else {
-                chrome.storage.local.set({ 'user_id': content.id });
-                processLogIn();
-            }
-        }
-    };
-});
-
-function rgb2hex(rgb) {
-    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
-
-    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
-    }
-    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-}
-
+////* POSTS FUNCTIONS *////
 function processUserPosts(content) {
 
 }
@@ -179,6 +133,60 @@ function getUserPosts() {
 
 }
 
+////* COLOR FUNCTIONS: *////
+function setupColors() {
+    // Put the color divs out there
+    for(i = 0; i < colors.length; i++) {
+        var colorDiv = document.createElement("div");
+        colorDiv.className = "colorButton";
+        colorDiv.id = colors[i];
+        colorDiv.style.backgroundColor = colors[i];
+        colorDiv.addEventListener("click", function(e) {
+            e.preventDefault();
+            var caller = e.target || e.srcElement;
+
+            // Set the color to the same as the background color. 
+            chrome.storage.local.set({'selectedColor': rgb2hex(caller.style.backgroundColor)}, function() {
+                console.log(rgb2hex(caller.style.backgroundColor) + " saved to default.");
+            });
+            changeSelectedStyleTo(caller);    
+        })
+        document.getElementById("colors").appendChild(colorDiv);
+    }
+
+    // Get (or set) the color div based on the currently stored setting.
+    chrome.storage.local.get('selectedColor', function (items) {
+        for(var prop in items) {
+            if(items.hasOwnProperty(prop))
+            {
+                // Move the "selected" style to the saved color
+                changeSelectedStyleTo(document.getElementById(items.selectedColor));
+                // Update the local colors
+                setElementColors(items.selectedColor);
+                return;
+            }
+        }
+        // If there is no setting for selectedColor - i.e. the first time popup.html is opened:
+        chrome.storage.local.set({'selectedColor': colors[0]}, function() {
+            console.log(colors[0] + " saved to default.");
+        });
+        changeSelectedStyleTo(document.getElementById(colors[0]));
+        setElementColors(colors[0]);
+    })
+}
+
+function changeSelectedStyleTo(element) {
+        // Remove selection styling:
+        var colors = document.getElementById("colors").getElementsByTagName("div");
+        for (i = 0; i < colors.length; i++)
+        {
+            if(colors[i].classList.contains("selectedColorButton"))
+                colors[i].classList.remove("selectedColorButton");
+        }
+        // Add selection styling to selected div:
+        element.className += " selectedColorButton";
+}
+
 function setElementColors(c) {
     var a = document.styleSheets;
     for (var i in a) if (a.hasOwnProperty(i)) {
@@ -219,4 +227,14 @@ function setTextColor(textElm, c) {
 
 function setBackgroundColor(elm, c) {
     elm.style.backgroundColor = c;
+}
+
+function rgb2hex(rgb) {
+    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
